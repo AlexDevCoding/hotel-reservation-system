194ccb636e9b4c1180a90c2b49369c875017aefc
AlexDevCoding <henriquezjosue384@gmail.com>
2025-05-31 15:31:22 -0400

    asdasd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,10 @@
- 
+ module.exports = {
+  content: [
+    "./src/Views/**/*.php",     // Vistas del sistema
+    "./public/assets/js/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
